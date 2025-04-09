cc20bacafb49902c43922cef2832241186057c90
Isaiah <isaiahlebg@gmail.com>
2025-04-09 11:37:52 +0200

    DESO PR 35 LIGNE MAIS HEREDOC MARCHE

--- builtins/heredoc.c
+++ builtins/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: irobinso <irobinso@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 09:50:37 by irobinso          #+#    #+#             */
-/*   Updated: 2025/04/09 10:52:15 by irobinso         ###   ########.fr       */
+/*   Updated: 2025/04/09 11:37:40 by irobinso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,6 @@ void	handle_heredoc(char *delimiter, int *infile)
 		perror("minishell: pipe");
 		exit(EXIT_FAILURE);
 	}
-
 	while (1)
 	{
 		line = readline("heredoc > ");
