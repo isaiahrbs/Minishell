2c313a9a0be85a8e20bf091975291ca686e1efd8
Dianka Matayi <dimatayi@c2r5s8.42lausanne.ch>
2025-04-25 13:57:43 +0200

    free things

---
+++ functions/free_functions.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free_functions.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dimatayi <dimatayi@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/25 11:38:03 by irobinso          #+#    #+#             */
+/*   Updated: 2025/04/25 13:19:38 by dimatayi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/minishell.h"
+
+void	free_list(char **list)
+{
+	int	i;
+
+	i = 0;
+	while (list[i])
+	{
+		free(list[i]);
+		i++;
+	}
+	free(list);
+}
+
+void	ft_free(char **executable, char ***cmd)
+{
+	if (*executable)
+	{
+		free(*executable);
+		*executable = NULL;
+	}
+	if (*cmd)
+		free_double_ptr(*cmd);
+}
